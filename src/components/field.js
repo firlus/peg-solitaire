8597f355c1c6802760807cf20a5704561403c7a9
Michael Firlus <michael@firlus.dev>
2022-06-22 17:49:26 +0200

    More responsiveness

--- src/components/field.js
+++ src/components/field.js
@@ -1,6 +1,9 @@
 export default function Field({ state, onClick }) {
   return (
-    <div className="h-12 w-12 md:h-16 md:w-16 m-2" onClick={onClick}>
+    <div
+      className="h-10 w-10 sm:h-12 sm:w-12 md:h-16 md:w-16 m-2"
+      onClick={onClick}
+    >
       {state !== " " ? (
         <div
           className={"rounded-full w-full h-full ".concat(
