f4ef0cd44962809d585a4f1c659dc577cb01d85f
Michael Firlus <michael@firlus.dev>
2022-06-22 15:18:43 +0200

    Added responsiveness and changed title

--- src/components/field.js
+++ src/components/field.js
@@ -1,6 +1,6 @@
 export default function Field({ state, onClick }) {
   return (
-    <div className="h-16 w-16 m-2" onClick={onClick}>
+    <div className="h-12 w-12 md:h-16 md:w-16 m-2" onClick={onClick}>
       {state !== " " ? (
         <div
           className={"rounded-full w-full h-full ".concat(
